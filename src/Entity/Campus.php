b34a1446513102935ca20be01e8323774d54e274
llebian2020 <laure.lebian2020@campus-eni.fr>
2021-03-03 10:24:13 +0100

    commit entity participant et campus

---
+++ src/Entity/Campus.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CampusRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CampusRepository::class)
+ */
+class Campus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column (type="string")
+     */
+    private $nomCampus;
+
+
+
+    /**
+     * @return mixed
+     */
+    public function getNomCampus()
+    {
+        return $this->nomCampus;
+    }
+
+    /**
+     * @param mixed $nomCampus
+     */
+    public function setNomCampus($nomCampus): void
+    {
+        $this->nomCampus = $nomCampus;
+    }
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
