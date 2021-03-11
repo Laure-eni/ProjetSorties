f27871e011618459ff45549e9dc1f272cca88191
Marianne Malherbe <marianne5102@gmail.com>
2021-03-11 20:23:38 +0100

    logout + nav

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller;
 
+use App\Entity\Sortie;
+use App\Form\CreerSortieType;
 use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -18,10 +21,25 @@ class SortieController extends AbstractController
     {
         $sorties = $repository->findSearch();
         return $this->render('sortie.html.twig', [
-           'sorties' => $sorties,
+            'sorties' => $sorties,
         ]);
-
-
-
     }
+
+
+    /**
+     * @Route ("/sorties/add", name="creation_sortie")
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function add(EntityManagerInterface $em)
+    {
+        $sortie = new sortie();
+        $sortieForm = $this->createForm(CreerSortieType::class, $sortie);
+
+        return $this->render('Form/add.html.twig', [
+            "sortieForm" => $sortieForm->createView()
+        ]);
+    }
+
+
 }
